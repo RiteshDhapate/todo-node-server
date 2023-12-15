3240f02c3f7f1ac3371433922e20af80dc5714fb
Ritesh Dhapate <148733786+RiteshDhapate@users.noreply.github.com>
2023-12-15 20:25:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const dbConnection =async()=>{
 }
 dbConnection();
 app.use(express.json());
-app.use(cors({
-    origin:'https://todo-react-app-ritesh.vercel.app',
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // allow session cookies from the browser to be sent
-    optionsSuccessStatus: 204
-}));
+const corsOptions = {
+  origin: 'https://todo-react-app-ritesh.vercel.app', // Replace with your client's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 204, // Some legacy browsers (IE11) may not understand 204
+};
+
+app.use(cors(corsOptions));
 app.use("/",route);
 const PORT = process.env.PORT || 2000
 app.listen(PORT,()=>console.log("server is running 2000 port"))
